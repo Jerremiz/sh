8375ff136da376d55bb12d98edb92c1bd675cb0c
Jerremiz <130783246+Jerremiz@users.noreply.github.com>
2026-06-12 20:58:00 +0800

    Update install_cockpit.sh

--- install_cockpit.sh
+++ install_cockpit.sh
@@ -54,10 +54,10 @@ cd ~/Downloads
 
 # 下载并解压 cockpit 插件
 echo "下载并解压 cockpit 插件"
-wget https://github.com/cockpit-project/cockpit-files/releases/download/27/cockpit-files-27.tar.xz # 文件管理器
+wget https://github.com/cockpit-project/cockpit-files/releases/download/27/cockpit-files-41.tar.xz # 文件管理器
 wget https://github.com/Jerremiz/cockpit-docker/releases/download/17/cockpit-docker-17.tar.xz # docker面板
 
-tar -xf cockpit-files-27.tar.xz
+tar -xf cockpit-files-41.tar.xz
 tar -xf cockpit-docker-17.tar.xz
 
 # 安装 cockpit 插件
